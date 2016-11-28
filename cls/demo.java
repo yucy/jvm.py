ad934c625ea22036813f19780aa260774f3141a2
unknown <s1672@wowshga.internal>
2016-11-28 18:41:57 +0800

    hh

--- cls/demo.java
+++ cls/demo.java
@@ -2,9 +2,11 @@ package cls;
 
 public class demo{
 
+	// static long j = 2,i = 1;
+	
 	public static void main(String[] args){
-		int i = 1;
-		int j = 2;
+		long i = 1;
+		long j = 2;
 		if(i>j){
 			System.out.println("bigger");
 		}else{
